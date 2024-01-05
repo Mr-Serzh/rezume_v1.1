c1dfec80f69366c265111c04f80fcddfad797d39
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2024-01-06 00:15:56 +0200

    add soft and tech skills data

---
+++ data/softSkills.js
@@ -0,0 +1,10 @@
+export const softSkills = [
+  'Scrum',
+  'Agile',
+  'Teamwork',
+  'Responsible and organized',
+  'Compliance with deadlines',
+  'Ready to take initiative',
+  'Attentive to details',
+  'Open to new knowledge'
+]
---
+++ data/techSkills.js
@@ -0,0 +1,17 @@
+export const techSkills = [
+  'React.js',
+  'Redux',
+  'MobX',
+  'Node.js',
+  'MongoDB',
+  'JavaScript',
+  'WebPack',
+  'Sass',
+  'CSS3',
+  'HTML5',
+  'Photoshop',
+  'Figma',
+  'VS Code',
+  'GIT',
+  'GitHub'
+]
