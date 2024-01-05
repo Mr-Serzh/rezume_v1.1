496803a396e7d35f61e5848b1ce3e5789e663c48
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2024-01-06 01:01:45 +0200

    add logic fo skills list

---
+++ js/script.js
@@ -0,0 +1,22 @@
+import { techSkills } from '/data/techSkills.js';
+import { softSkills } from '/data/softSkills.js';
+
+function createSkillsList (skillArray, listElementId){
+  const skillsList = document.getElementById(listElementId)
+
+  skillArray.forEach(item => {
+    const listItem = document.createElement('li')
+    const spanItem = document.createElement('span')
+
+    listItem.classList.add('skills-item')
+    spanItem.classList.add('skills-text')
+
+    spanItem.textContent = item
+
+    listItem.appendChild(spanItem)
+    skillsList.appendChild(listItem)
+  })
+}
+
+createSkillsList(techSkills, 'tech-skills-list')
+createSkillsList(softSkills, 'soft-skills-list')
